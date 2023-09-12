fab2b50b8390daf6f5e6fd1905f3aab95dd4ea2e
Raphae Kiffer <kiffer.raphael@gmail.com>
2023-09-12 10:40:01 +0200

    Add base MaterialApp with text style. Add home screen with hello text properly styled

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Colors.amber,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          'Hello',
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+      ),
+    );
+  }
 }
