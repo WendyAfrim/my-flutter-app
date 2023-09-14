4a96e0aa7c98c5e0a03f70c8e8ed47a4a0991b79
Raphae Kiffer <kiffer.raphael@gmail.com>
2023-09-14 11:42:04 +0200

    Example with double stateful widget

--- lib/square.dart
+++ lib/square.dart
@@ -8,11 +8,12 @@ class Example extends StatefulWidget {
 }
 
 class _ExampleState extends State<Example> {
-  Color _color = Colors.red;
+  Color _color = Colors.yellow;
 
   void _changeColor() {
     print('changeColor');
-    setState(() => _color = _color == Colors.red ? Colors.blue : Colors.red);
+    setState(
+        () => _color = _color == Colors.yellow ? Colors.green : Colors.yellow);
   }
 
   @override
@@ -30,19 +31,46 @@ class _ExampleState extends State<Example> {
   }
 }
 
-class Square extends StatelessWidget {
+class Square extends StatefulWidget {
   final Color color;
 
   const Square({super.key, required this.color});
 
   @override
+  State<Square> createState() => _SquareState();
+}
+
+class _SquareState extends State<Square> {
+  Color _color = Colors.red;
+
+  void _changeColor() {
+    print('changeColor');
+    setState(() => _color = _color == Colors.red ? Colors.blue : Colors.red);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _color = widget.color;
+  }
+
+  @override
+  void didUpdateWidget(Square oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _color = widget.color;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: color,
+    return GestureDetector(
+      onTap: _changeColor,
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: _color,
+        ),
       ),
     );
   }
